d32f78e6c3693b63ea64eb33244ba683c8307bd0
Pranjal Chanda <pchanda@hu-pchanda-blr.qualcomm.com>
2022-05-20 22:07:07 +0530

    CQ Enqueue Success

--- Circular_Queue/cq.c
+++ Circular_Queue/cq.c
@@ -1,2 +1,53 @@
 #include "cq.h"
 
+int cq_enqueue(cq_t * p_cq, uint8_t * p_element)
+{
+
+    if ((p_cq->tail + p_cq->element_size) % p_cq->size == p_cq->head) // condition to check queue is full
+    {
+        p_cq->tail += p_cq->element_size;
+        p_cq->tail %= p_cq->size;
+        p_cq->head += p_cq->element_size;
+        p_cq->head %= p_cq->size;
+    }
+    else
+    {
+        if (p_cq->tail == (size_t)-1)   // First Element
+        {
+            p_cq->tail = 0;
+        }
+        else
+        {
+            p_cq->tail += p_cq->element_size;
+        }
+        p_cq->tail %= p_cq->size;
+    }
+    memcpy(p_cq->p_buffer + p_cq->tail, p_element, p_cq->element_size);
+    
+    return CQ_SUCCESS;
+}
+int cq_dequeue(cq_t *p_cq, uint8_t *p_element)
+{
+    memcpy(p_element, p_cq->p_buffer + p_cq->tail, p_cq->element_size);
+    if (p_cq->head == p_cq->tail)
+    {
+        /* Queue Empty */
+        return CQ_FAILURE;
+    }
+    else
+    {
+        p_cq->tail -= p_cq->element_size;
+        p_cq->tail %= p_cq->size;
+    }
+    return CQ_SUCCESS;
+}
+int cq_peak(cq_t *p_cq, uint8_t *p_element)
+{
+    if (p_cq->head == p_cq->tail)
+    {
+        /* Queue Empty */
+        return CQ_FAILURE;
+    }
+    memcpy(p_element, p_cq->p_buffer + p_cq->tail, p_cq->element_size);
+    return CQ_SUCCESS;
+}
--- Circular_Queue/cq.h
+++ Circular_Queue/cq.h
@@ -1,18 +1,38 @@
 #ifndef _CQ_H_
 #define _CQ_H_
 
-#define CONCAT_2(a,b)           a##b
-#define CONCAT_3(a,b,c)         a##b##c
+#include <inttypes.h>
+#include <string.h>
 
-typedef struct cq_attr
+#define CONCAT_2(a, b) a##b
+#define CONCAT_3(a, b, c) a##b##c
+
+#define CQ_SUCCESS 0U
+#define CQ_FAILURE 1U
+
+typedef struct cq
 {
-    void *p_buffer;
+    uint8_t *p_buffer;
     size_t size;
-} cq_attr_t;
+    size_t element_size;
+    size_t head;
+    size_t tail;
+} cq_t;
 
-#define circular_queue_init(NAME, size, type)  \
-    type CONCAT_2(NAME, _cq_buffer)[size + 1]; \
-    cq_attr_t CONCAT_3(cq_, NAME, _attr);
+#define CQ_INIT_BUFFER(_NAME, _size, _type)                                  \
+    static uint8_t CONCAT_2(_NAME, _cq_buffer)[(_size * sizeof(_type)) + 1]; \
+    static cq_t CONCAT_3(cq_, _NAME, _inst) =                                \
+        {                                                                    \
+            .p_buffer = CONCAT_2(_NAME, _cq_buffer),                         \
+            .size = (_size * sizeof(_type)),                                 \
+            .element_size = sizeof(_type),                                   \
+            .head = 0,                                                       \
+            .tail = -1};
 
+#define CQ_INSTANCE(_NAME) CONCAT_3(cq_, _NAME, _inst)
+
+int cq_enqueue(cq_t *p_cq, uint8_t *p_element);
+int cq_dequeue(cq_t *p_cq, uint8_t *p_element);
+int cq_peak(cq_t *p_cq, uint8_t *p_element);
 
 #endif
